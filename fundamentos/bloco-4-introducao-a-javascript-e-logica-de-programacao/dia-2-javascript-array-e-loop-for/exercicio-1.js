ddbbb11009af109cbfba28eda6f84cd08417e5d7
José Diogo Lobato Ribeiro <jdiogolobato@gmail.com>
2022-08-19 23:30:03 -0300

    Feito até a parte 4

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-1.js
@@ -0,0 +1,44 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+
+// 1 - Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log();
+
+// for (let index = 0; index < numbers.length; index += 1){
+//     console.log(numbers[index])
+// }
+
+// 2 - Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+
+// let sumNumbers = 0;
+
+// for (let index = 0; index < numbers.length; index +=1){
+//     sumNumbers += numbers[index];
+// }
+//     console.log(sumNumbers);
+
+// 3 - Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+
+// let sumNumbers = 0;
+
+// for (let index = 0; index < numbers.length; index +=1){
+//     sumNumbers += numbers[index];
+// }
+// let medNumbers = sumNumbers / numbers.length;
+
+// console.log(medNumbers);
+
+// 4 - Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
+// let sumNumbers = 0;
+
+// for (let index = 0; index < numbers.length; index +=1){
+//     sumNumbers += numbers[index];
+// }
+
+// let medNumbers = sumNumbers / numbers.length;
+
+// if (medNumbers > 20){
+//     console.log("valor maior que 20");
+// } else {
+//     console.log ("valor menor ou igual a 20");
+// }
