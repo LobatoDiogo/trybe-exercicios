92280abbc5d04e5681f7b1fff326cd2cb645c18a
José Diogo Lobato Ribeiro <jdiogolobato@gmail.com>
2022-09-25 20:37:52 -0300

    Segundo exercício pronto

--- fundamentos/secao-8-JavaScript/dia-1-introducao-a-higher-order-functions/hofs.js
+++ fundamentos/secao-8-JavaScript/dia-1-introducao-a-higher-order-functions/hofs.js
@@ -17,3 +17,17 @@ const newEmployees = (novaPessoa) => {
     return { nomeCompleto , email: `${email}@trybe.com` };
   };
   console.log(newEmployees(novaPessoa));
+
+//  --- Sorteador de loteria ---
+// Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: “Tente novamente” ou “Parabéns você ganhou”).
+
+const numerosIguais = (numeroSorteado, meuNumero) => meuNumero === numeroSorteado;
+
+const resultadoLoteria = (meuNumero, callback) => {
+    const Numero = Math.ceil(Math.random() * 5);
+    return callback(meuNumero, Numero) ? 'Parabéns você ganhou' : 'Tente novamente';
+};
+console.log(resultadoLoteria(3, numerosIguais));
+
+// const Numero = Math.ceil(Math.random() * 5);
+// console.log(Numero);
